68d1e7e9ed6cebfec357ccd172ef644d6d0896fe
rogissart <m.rogissart@gmail.com>
2023-09-13 20:17:19 +0200

    Add second endpoint

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -7,6 +7,14 @@ class PricesController < ApplicationController
     render json: @dated_prices.sort_by{|price|price[:time]}
   end
 
+  def biggest
+    params = price_params
+    dated_prices = Price.all.select{ |price| price[:date] == params[:date]}
+    valid_pairs = dated_prices.combination(2).to_a.select { |pair| pair[0][:time] < pair[1][:time] && pair[0][:value] > pair[1][:value] }
+    best_pair = valid_pairs.max_by { |pair| pair[0][:value] - pair[1][:value] }
+    profit = best_pair[0][:value] - best_pair[1][:value]
+    render json: "%d€" %[profit * 100]
+  end
 
   private
   def price_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :prices
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :prices, only: :index
+  get 'prices/profit', to: 'prices#biggest'
 end
